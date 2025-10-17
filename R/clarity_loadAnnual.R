1f7b7ad89eb5829a3221570fb6edd62079f08b53
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2025-10-16 18:05:21 -0700

    WIP:  clarity_loadAnnual()

---
+++ R/clarity_loadAnnual.R
@@ -0,0 +1,198 @@
+#' @export
+#' @importFrom dplyr across
+#'
+#' @title Load annual Clarity monitoring data
+#'
+#' @param year Year [YYYY].
+#' @param archiveBaseUrl Base URL for monitoring v2 data files.
+#' @param archiveBaseDir Local base directory for monitoring v2 data files.
+#' @param QC_negativeValues Type of QC to apply to negative values.
+#' @param QC_removeSuspectData Removes monitors determined to be misbehaving.
+#'
+#' @return A \emph{mts_monitor} object with Clarity data. (A list with
+#' \code{meta} and \code{data} dataframes.)
+#'
+#' @description Loads pre-generated .rda files containing annual
+#' Clarity data.
+#'
+#' If \code{archiveDataDir} is defined, data will be loaded from this local
+#' archive. Otherwise, data will be loaded from the monitoring data repository
+#' maintained by the USFS AirFire team.
+#'
+#' Current year files loaded by this function are updated once per week.
+#'
+#' For the most recent data in the last 10 days, use \code{clarity_loadLatest()}.
+#'
+#' For daily updates covering the most recent 45 days, use \code{clarity_loadDaily()}.
+#'
+#' @seealso \code{\link{clarity_loadDaily}}
+#'
+#' @examples
+#' \dontrun{
+#' library(AirMonitor)
+#'
+#' # Fail gracefully if any resources are not available
+#' try({
+#'
+#' # See https://en.wikipedia.org/wiki/Snake_River_Complex_Fire
+#'
+#' # Clarity monitors during the Snake River Complex Fire
+#' clarity_loadAnnual(2021) \%>\%
+#'   monitor_filter(stateCode \%in\% c("ID", "MT")) \%>\%
+#'   monitor_filterDate(20210707, 20210820, timezone = "America/Denver") \%>\%
+#'   monitor_timeseriesPlot(
+#'     ylim = c(0, 300),
+#'     xpd = NA,
+#'     addAQI = TRUE,
+#'     main = "Clarity monitors during Snake River Complex Fire"
+#'   )
+#'
+#' }, silent = FALSE)
+#' }
+
+clarity_loadAnnual <- function(
+  year = NULL,
+  archiveBaseUrl = paste0(
+    "https://airfire-data-exports.s3.us-west-2.amazonaws.com/",
+    "sensors/v3/PM2.5"
+  ),
+  archiveBaseDir = NULL,
+  QC_negativeValues = c("zero", "na", "ignore"),
+  QC_removeSuspectData = TRUE
+) {
+
+  parameterName <- "PM2.5"
+
+  # ----- Validate parameters --------------------------------------------------
+
+  MazamaCoreUtils::stopIfNull(year)
+  MazamaCoreUtils::stopIfNull(parameterName)
+
+  if ( as.numeric(year) < 2023 )
+    stop(paste0("No Clarity data is available before 2023"))
+
+  QC_negativeValues <- match.arg(QC_negativeValues)
+
+  if ( is.null(archiveBaseUrl) && is.null(archiveBaseDir) )
+    stop("one of 'archiveBaseUrl' or 'archiveBaseDir' must be defined")
+
+  # Parameter code
+  validParameterNames <- c(
+    # "BARPR",
+    # "BC",
+    # "CO",
+    # "NO",
+    # "NO2",
+    # "NO2Y",
+    # "NO2X",
+    # "NOX",
+    # "NOOY",
+    # "OC",
+    # "OZONE",
+    # "PM10",
+    "PM2.5"
+    # "PM2.5_nowcast"
+    # "PRECIP",
+    # "RHUM",
+    # "SO2",
+    # "SRAD",
+    # "TEMP",
+    # "UV-AETH",
+    # "WD",
+    # "WS"
+  )
+
+  parameterName <- as.character(parameterName)
+  if ( !parameterName %in% validParameterNames ) {
+    stop(sprintf(
+      "data for parameterName '%s' has not been processed",
+      parameterName
+    ))
+  }
+
+  # ----- Load data ------------------------------------------------------------
+
+  # Create file name and path according to the AirMonitorIngest scheme
+
+  if ( is.null(archiveBaseUrl) ) {
+    dataUrl <- NULL
+  } else {
+    dataUrl <- file.path(archiveBaseUrl, "clarity", year, "data")
+  }
+
+  if ( is.null(archiveBaseDir) ) {
+    dataDir <- NULL
+  } else {
+    dataDir <- file.path(archiveBaseDir, "clarity", year, "data")
+  }
+
+  metaFileName <- sprintf("clarity_%s_%s_meta.rda", parameterName, year)
+  dataFileName <- sprintf("clarity_%s_%s_data.rda", parameterName, year)
+
+  meta <- MazamaCoreUtils::loadDataFile(metaFileName, dataUrl, dataDir)
+  data <- MazamaCoreUtils::loadDataFile(dataFileName, dataUrl, dataDir)
+
+  # Guarantee that 'meta' and 'data' match
+  ids <- names(data)[-1]
+
+  meta <-
+    meta %>%
+    dplyr::filter(.data$deviceDeploymentID %in% ids)
+
+  # Guarantee presence of fullAQSID and postalCode
+  if ( !"fullAQSID" %in% names(meta) ) meta$fullAQSID <- NA_character_
+  if ( !"postalCode" %in% names(meta) ) meta$postalCode <- NA_character_
+
+  data <-
+    data %>%
+    dplyr::select(dplyr::all_of(c("datetime", meta$deviceDeploymentID))) %>%
+    # Replace any NaN that snuck in
+    dplyr::mutate(across(tidyselect::vars_select_helpers$where(is.numeric), function(x) ifelse(is.nan(x), NA, x)))
+
+  # Create monitor object
+  monitor <- list(meta = meta, data = data)
+
+  monitor <- structure(monitor, class = c("mts_monitor", "mts", class(monitor)))
+
+  MazamaTimeSeries::mts_check(monitor)
+
+  # ----- Apply QC -------------------------------------------------------------
+
+  # Handle negative values
+  if ( QC_negativeValues == "zero" ) {
+    monitor <- monitor_replaceValues(monitor, data < 0, 0)
+  } else if ( QC_negativeValues == "na" ) {
+    monitor <- monitor_replaceValues(monitor, data < 0, as.numeric(NA))
+  }
+
+  # NOTE:  Several monitors in 2015 have values only at 0, 1000, 2000, 3000, ...
+  if ( QC_removeSuspectData ) {
+
+    monitor <-
+      monitor %>%
+      monitor_mutate(QC_invalidateConsecutiveSuspectValues) %>%
+      monitor_dropEmpty()
+
+  }
+
+  # ----- Return ---------------------------------------------------------------
+
+  return(monitor)
+
+}
+
+# ===== DEBUG ==================================================================
+
+if ( FALSE ) {
+
+
+  year <- 2023
+  archiveBaseUrl <- "https://airfire-data-exports.s3.us-west-2.amazonaws.com/sensors/v3/PM2.5"
+  archiveBaseDir <- NULL
+  QC_negativeValues <- "zero"
+  QC_removeSuspectData <- TRUE
+
+
+
+
+}
